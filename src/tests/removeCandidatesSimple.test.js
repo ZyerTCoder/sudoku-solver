19e8a561ab6e8462948a13c21c2186630c5ddadd
ZyerTCoder <protekthorwow@gmail.com>
2023-10-07 13:40:05 +0100

    removeCandidatesSimple test todos setup

---
+++ src/tests/removeCandidatesSimple.test.js
@@ -0,0 +1,39 @@
+import { Sudoku } from "../scripts/sudoku"
+import { beforeEach, expect, test } from "vitest"
+
+test.todo("removeCandidatesSimple 1 cell top left box", () => {
+	const sudoku = new Sudoku("1................................................................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell top box", () => {
+	const sudoku = new Sudoku("....2............................................................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell top right box", () => {
+	const sudoku = new Sudoku("........3........................................................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell left box", () => {
+	const sudoku = new Sudoku("....................................4............................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell center box", () => {
+	const sudoku = new Sudoku("........................................5........................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell right box", () => {
+	const sudoku = new Sudoku("............................................6....................................")
+})
+
+test.todo("removeCandidatesSimple 1 cell bottom left box", () => {
+	const sudoku = new Sudoku("........................................................................7........")
+})
+
+test.todo("removeCandidatesSimple 1 cell bottom box", () => {
+	const sudoku = new Sudoku("............................................................................8....")
+})
+
+test.todo("removeCandidatesSimple 1 cell bottom right box", () => {
+	const sudoku = new Sudoku("................................................................................9")
+})
+
