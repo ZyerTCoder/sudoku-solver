3835ee73814c30182cb40b9ef0e57012bcaf28a4
ZyerTCoder <protekthorwow@gmail.com>
2023-10-07 13:38:16 +0100

    removeCandidatesSimple implemented

---
+++ src/scripts/removeCandidatesSimple.js
@@ -0,0 +1,40 @@
+let floor = Math.floor
+
+export default function removeCandidatesSimple(board) {
+	let out = []
+
+	for (let row=0; row<9; row++) {
+		for (let col=0; col<9; col++) {
+			if (typeof(board.board[row][col]) !== "number") {
+				// // check row
+				for (let rr=0; rr<9; rr++) {
+					if (typeof(board.board[rr][col]) === "number") {
+						let cellValue = board.board[rr][col]
+						out.push({row:row, col:col, cand: cellValue, type:"rm"})
+					}
+				}
+
+				// // check col
+				for (let cc=0; cc<9; cc++) {
+					if (typeof(board.board[row][cc]) === "number") {
+						let cellValue = board.board[row][cc]
+						out.push({row:row, col:col, cand: cellValue, type:"rm"})
+					}
+				}
+				
+				// check box
+				let x = floor(row/3)
+				let y = floor(col/3)
+				for (let rr=x*3; rr<x*3+3; rr++) {
+					for (let cc=y*3; cc<y*3+3; cc++) {
+						if (typeof(board.board[rr][cc]) === "number") {
+							let cellValue = board.board[rr][cc]
+							out.push({row:row, col:col, cand: cellValue, type:"rm"})
+						}
+					}
+				}
+			}
+		}
+	}
+	return out
+}
