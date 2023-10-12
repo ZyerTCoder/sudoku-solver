3b88adb6dd720fd08e62ec19f8d74b8551e1f31b
ZyerTCoder <protekthorwow@gmail.com>
2023-10-12 17:19:54 +0100

    updated to use the dict version of unsolved cells + test for loading a fully empty sudoku

--- src/tests/sudoku.test.js
+++ src/tests/sudoku.test.js
@@ -2,10 +2,11 @@ import { Sudoku } from "../scripts/sudoku"
 import { assertType, beforeEach, expect, expectTypeOf, test } from "vitest"
 
 const easy_board = "...1.5...14....67..8...24...63.7..1.9.......3.1..9.52...72...8..26....35...4.9..."
-const unsolvedCellStr = JSON.stringify([
-	true, true, true,
-	true, true, true,
-	true, true, true])
+const unsolvedCellStr = JSON.stringify({
+	1: true, 2: true, 3: true,
+	4: true, 5: true, 6: true,
+	7: true, 8: true, 9: true,
+})
 
 test("normal sudoku object creation from string", () => {
 	const sudoku = new Sudoku(easy_board)
@@ -21,7 +22,6 @@ test("normal sudoku object creation from string", () => {
 	}
 
 	// correctly placed values
-	expect(JSON.stringify(sudoku.board[0][0])).toBe(unsolvedCellStr)
 	expect(JSON.stringify(sudoku.board[4][2])).toBe(unsolvedCellStr)
 	expect(JSON.stringify(sudoku.board[8][8])).toBe(unsolvedCellStr)
 	expect(sudoku.board[1][1]).toBe(4)
@@ -54,6 +54,28 @@ test("completed sudoku object creation from string", () => {
 	expect(sudoku.board[8][0]).toBe(9)
 	expect(sudoku.board[8][8]).toBe(2)
 	expect(sudoku.board[5][3]).toBe(8)
+})
+
+test("loading fully empty sudoku", () => {
+	const sudoku = new Sudoku(".................................................................................")
+
+	// unsolvedCells logic
+	expect(sudoku.unsolvedCells).toBe(81)
+	expect(() => sudoku.unsolvedCells = 1).toThrowError(TypeError)
+
+	// structure
+	expect(sudoku.board.length).toBe(9)
+	for (let row=0; row<9; row++) {
+		expect(sudoku.board[row].length).toBe(9)
+	}
+
+	// correctly placed values
+	for (let row=0; row<9; row++) {
+		for (let col=0; col<9; col++) {
+			let c = JSON.stringify(sudoku.board[row][col])
+			expect(c).toBe(unsolvedCellStr)
+		}
+	}
 })
 
 test("new sudoku from invalid input fails", () => {
