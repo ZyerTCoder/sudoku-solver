abcb9fc2d7f3b12ad2af27f80f93ed6ee57671b3
ZyerTCoder <protekthorwow@gmail.com>
2023-10-24 07:01:44 +0100

    added end to end tests

---
+++ src/tests/E2ESolves.test.js
@@ -0,0 +1,43 @@
+import exampleSudokus from "../scripts/exampleSudokus"
+import { Sudoku } from "../scripts/sudoku"
+import { expect, test } from "vitest"
+
+const IterationCap = 100000
+
+test("E2E: Easy board", () => {
+	const sudoku = new Sudoku(exampleSudokus["Easy"])
+	let result
+	let iterations = 0
+	do {
+		result = sudoku.next()
+		iterations++
+	} while (result && iterations < IterationCap)
+	expect(iterations).toBeLessThan(IterationCap)
+	expect(result).toBe(0)
+})
+
+test("E2E: Invalid board", () => {
+	const sudoku = new Sudoku(exampleSudokus["Invalid Board"])
+	let result
+	let iterations = 0
+	do {
+		result = sudoku.next()
+		iterations++
+		if (result === "no tech") { break }
+	} while (result && iterations < IterationCap)
+	expect(result).toBe("no tech")
+	expect(iterations).toBeLessThan(IterationCap)
+})
+
+test("E2E: Hidden Singles", () => {
+	const sudoku = new Sudoku(exampleSudokus["Hidden Singles"])
+	let result
+	let iterations = 0
+	do {
+		result = sudoku.next()
+		iterations++
+	} while (result && iterations < IterationCap)
+	expect(iterations).toBeLessThan(IterationCap)
+	expect(result).toBe(0)
+})
+
