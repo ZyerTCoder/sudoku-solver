13bbb2f22efa139dea678adfba732d426e40554f
ZyerTCoder <protekthorwow@gmail.com>
2023-10-29 14:27:04 +0000

    finished naked candidates testing

--- src/tests/nakedCandidates.test.js
+++ src/tests/nakedCandidates.test.js
@@ -121,36 +121,6 @@ test("nakedCandidates2: in box", () => {
 	])
 })
 
-test.todo("nakedCandidates2: adjacent pair in same row triggers both row and box", () => {
-	const sudoku = new Sudoku("TEST_NO_CANDIDATES")
-	for (let col=0; col<2; col++) {
-		sudoku.testingSetCandidatesOnCell(0, col, {
-			1: true, 2: true, 3: false,
-			4: false, 5: false, 6: false,
-			7: false, 8: false, 9: false,
-		})
-	}
-	sudoku.testingSetCandidatesOnCell(0, 2, {
-		1: false, 2: true, 3: true,
-		4: true, 5: false, 6: false,
-		7: false, 8: false, 9: false,
-	})
-
-	let changes = nakedCandidates(sudoku, 2)
-	expect(changes).toStrictEqual([
-		{"row":0,"col":2,"cand":2,"type":"rm","reason":"NakedPair 12 in row"},
-		
-		{"row":0,"col":0,"cand":1,"type":"yellow"},
-		{"row":0,"col":0,"cand":2,"type":"yellow"},
-		{"row":0,"col":1,"cand":1,"type":"yellow"},
-		{"row":0,"col":1,"cand":2,"type":"yellow"},
-		
-		// this passes with this config but imo it should return instead just a
-		// single type:rm with reason:nakedPair 12 in row box
-		{"row":0,"col":2,"cand":2,"type":"rm","reason":"NakedPair 12 in box"},
-	])
-})
-
 test("nakedCandidates2: 2 pairs in same row", () => {
 	for (let col=0; col<9; col++) {
 		sudoku.testingSetCandidatesOnCell(0, col, {
@@ -308,7 +278,46 @@ test("nakedCandidates2: 2 pairs in same box", () => {
 	])
 })
 
-test.todo("nakedCandidates2: 2 pairs in diff row/col pointing at same cell", () => {})
+test("nakedCandidates2: 2 pairs in diff row/col (sqr) pointing at same cell", () => {
+	sudoku.testingSetCandidatesOnCell(0, 0, {
+		1: false, 2: true, 3: true,
+		4: true, 5: false, 6: false,
+		7: false, 8: false, 9: false,
+	})
+	for (let row=2; row<4; row++) {
+		sudoku.testingSetCandidatesOnCell(row, 0, {
+			1: true, 2: true, 3: false,
+			4: false, 5: false, 6: false,
+			7: false, 8: false, 9: false,
+		})
+	}
+	for (let col=2; col<4; col++) {
+		sudoku.testingSetCandidatesOnCell(0, col, {
+			1: true, 2: true, 3: false,
+			4: false, 5: false, 6: false,
+			7: false, 8: false, 9: false,
+		})
+	}
+
+	let changes = nakedCandidates(sudoku, 2)
+	expect(changes).toStrictEqual([
+		{"row":0,"col":0,"cand":2,"type":"rm","reason":"Naked Pair 12 in row"},
+		
+		{"row":0,"col":2,"cand":1,"type":"yellow"},
+		{"row":0,"col":2,"cand":2,"type":"yellow"},
+		{"row":0,"col":3,"cand":1,"type":"yellow"},
+		{"row":0,"col":3,"cand":2,"type":"yellow"},
+
+		{"row":0,"col":0,"cand":2,"type":"rm","reason":"Naked Pair 12 in col"},
+		
+		{"row":2,"col":0,"cand":1,"type":"yellow"},
+		{"row":2,"col":0,"cand":2,"type":"yellow"},
+		{"row":3,"col":0,"cand":1,"type":"yellow"},
+		{"row":3,"col":0,"cand":2,"type":"yellow"},
+
+		{"row":0,"col":0,"cand":2,"type":"rm","reason":"Naked Pair 12 in box"},
+	])
+})
 
 test("nakedCandidates3: in row", () => {
 	for (let col=0; col<9; col++) {
